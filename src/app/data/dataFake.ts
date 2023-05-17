aa499b1e5c5497cca4da454037a5688e0a537c68
Nickgalant <nicholasgalante@hotmail.co.uk>
2023-05-17 19:32:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"New Iron Man Coming",
+        "description":"Marvel studios announces new movie about Iron Man for 2025",
+        "photoCover":"https://cdn.marvel.com/content/1x/002irm_ons_mas_mob_01_0.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Séries Marvel 2022",
+        "description":"ola",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+]
